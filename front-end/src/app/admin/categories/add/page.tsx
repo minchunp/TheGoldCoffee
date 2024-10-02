cfe190957367000fea2239e874ad606b7c2d0ed8
HT Thuận <94607277+thuanht1999@users.noreply.github.com>
2024-10-02 17:27:28 +0700

    update CRUD cate admin

--- front-end/src/app/admin/categories/add/page.tsx
+++ front-end/src/app/admin/categories/add/page.tsx
@@ -1,63 +1,127 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import "../../../../../public/css/addProductAdmin.css";
 import "../../../../../public/css/dashboardAdmin.css";
 import BannerSectionAdmin from "../../../../../public/images/wallpaper-angledwares.jpg";
+import axios from "axios";
 
-const AddCate = () => {
-   return (
-      <>
-         <section>
-            <div className="main-dashboard">
-               <div className="boxcenter">
-                  <div className="title-product">
-                     <h1>Thêm danh mục</h1>
-                     <Link href="/admin/categories">
-                        <i className="bi bi-arrow-left-short"></i>
-                     </Link>
-                  </div>
-
-                  <div className="banner-product">
-                     <img src={BannerSectionAdmin.src} alt="" />
-                     <div className="text-banner">
-                        <p>CHÀO MỪNG BẠN ĐẾN VỚI</p>
-                        <h1>THE GOLD COFFEE</h1>
-                        <p>NĂM 2024</p>
-                     </div>
-                  </div>
-
-                  <div className="order-pending">
-                     <div className="title-order-pending">
-                        <h1>ID danh mục #Random</h1>
-                     </div>
-
-                     <div id="list-cate" className="list-order-pending">
-                        <div className="title-list-order-pending">
-                           <p>ID</p>
-                           <p>Ảnh danh mục</p>
-                           <p>Tên danh mục</p>
-                           <p>Chức năng</p>
-                        </div>
-
-                        <form>
-                           <div className="main-list">
-                              <div className="main-order-pending">
-                                 <a href="#">
-                                    <p>Random</p>
-                                 </a>
-                                 <input type="file" name="name_user" placeholder="Ảnh danh mục" />
-                                 <input type="text" name="email_user" placeholder="Tên danh mục" />
-                                 <button type="submit">
-                                    <i className="bi bi-check2"></i>
-                                 </button>
-                              </div>
-                           </div>
-                        </form>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </section>
-      </>
-   )
+interface Category {
+  _id: string;
+  img_cate: string;
+  name_cate: string;
+  status_cate: string;
 }
+
+const AddCate: React.FC = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [name, setName] = useState("");
+  const [image, setImage] = useState<File | null>(null);
+  const [status, setStatus] = useState(true);
+
+  const handleAddCategory = async () => {
+    try {
+      const imageName = image ? image.name : "";
+      const cateData = {
+        img_cate: imageName,
+        name_cate: name,
+        status_cate: status ? "1" : "0",
+      };
+      const response = await axios.post(
+        "http://localhost:3001/category/add",
+        cateData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Category added successfully", response.data);
+      window.location.href = "/admin/categories";
+    } catch (err) {
+      console.error("Error adding category:", err);
+    }
+  };
+  return (
+    <>
+      <section>
+        <div id="add-product-page" className="add-product">
+          <div className="boxcenter">
+            <div className="title-product">
+              <h1>Thêm Danh Mục</h1>
+              <Link href="/admin/categories">
+                <i
+                  id="return-main-product-btn"
+                  className="bi bi-arrow-left-short"></i>
+              </Link>
+            </div>
+
+            <div className="container-add-product">
+              <div className="box-demo-add">
+                <div className="box-featured-product">
+                  <div className="product-image">
+                    {!image && (
+                      <div className="box-toggle-image">
+                        <i className="bi bi-image"></i>
+                      </div>
+                    )}
+                    {image && ( // Hiển thị ảnh mới nếu có
+                      <img src={URL.createObjectURL(image)} alt="Product" />
+                    )}
+                  </div>
+                  <div className="product-content">
+                    <div className="product-name">
+                      <a href="#">
+                        <h3>{name || "Tên danh mục"}</h3>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="box-input-add">
+                <div className="element-input">
+                  <p>Tên danh mục</p>
+                  <input
+                    type="text"
+                    placeholder="Tên danh mục"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+
+                <div className="element-input">
+                  <p>Ảnh danh mục</p>
+                  <input
+                    type="file"
+                    onChange={(e) => {
+                      if (e.target.files) {
+                        setImage(e.target.files[0]);
+                      }
+                    }}
+                  />
+                </div>
+
+                <div className="element-input">
+                  <p>Status</p>
+                  <input
+                    type="checkbox"
+                    id="tagSale-product"
+                    checked={status}
+                    onChange={(e) => setStatus(e.target.checked)}
+                  />
+                </div>
+                <button id="add-product" onClick={handleAddCategory}>
+                  Thêm sản phẩm
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default AddCate;
--- front-end/src/app/admin/categories/page.tsx
+++ front-end/src/app/admin/categories/page.tsx
@@ -1,62 +1,107 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import "../../../../public/css/dashboardAdmin.css";
 import BannerSectionAdmin from "../../../../public/images/wallpaper-angledwares.jpg";
+import axios from "axios";
 
-const CategoryAdmin = () => {
-   return (
-      <>
-         <section>
-            <div className="main-dashboard">
-               <div className="boxcenter">
-                  <div className="title-product">
-                     <h1>Danh mục sản phẩm</h1>
-                     <Link href="/admin/categories/add">
-                        <i className="bi bi-plus"></i>
-                     </Link>
-                  </div>
-
-                  <div className="banner-product">
-                     <img src={BannerSectionAdmin.src} alt="" />
-                     <div className="text-banner">
-                        <p>CHÀO MỪNG BẠN ĐẾN VỚI</p>
-                        <h1>THE GOLD COFFEE</h1>
-                        <p>NĂM 2024</p>
-                     </div>
-                  </div>
-
-                  <div className="order-pending">
-                     <div className="title-order-pending">
-                        <h1>Danh sách danh mục</h1>
-                     </div>
-
-                     <div id="list-cate" className="list-order-pending">
-                        <div className="title-list-order-pending">
-                           <p>ID</p>
-                           <p>Ảnh danh mục</p>
-                           <p>Tên danh mục</p>
-                           <p>Function</p>
-                        </div>
-
-                        <div className="main-list">
-                           <div className="main-order-pending">
-                              <Link href="#!">
-                                 <p>ID</p>
-                              </Link>
-                              <img src={`${process.env.NEXT_PUBLIC_IMAGE_PRO_URL}1669736859_hi-tea-yuzu-tran-chau_400x400.png`} alt="" />
-                              <p>Tên danh mục</p>
-                              <div className="container-func">
-                                 <Link href="/admin/categories/edit"><button><i className="bi bi-gear"></i></button></Link>
-                                 <button><i className="bi bi-x-lg"></i></button>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </section>
-      </>
-   )
+interface Category {
+  _id: string;
+  img_cate: string;
+  name_cate: string;
+  status_cate: string;
 }
+
+const CategoryAdmin: React.FC = () => {
+  const [cate, setCate] = useState<Category[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Category[]>("http://localhost:3001/CategorysAPI/listCategory")
+      .then((response) => {
+        setCate(response.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching category:", err);
+      });
+  }, []);
+
+  const deleteCate = (id: string) => {
+    axios
+      .delete(`http://localhost:3001/category/delete/${id}`)
+      .then((response) => {
+        setCate((prevCate) => prevCate.filter((cate) => cate._id !== id));
+      })
+      .catch((err) => {
+        console.error("Error deleting Category:", err);
+      });
+  };
+  return (
+    <>
+      <section>
+        <div className="main-dashboard">
+          <div className="boxcenter">
+            <div className="title-product">
+              <h1>Danh mục sản phẩm</h1>
+              <Link href="/admin/categories/add">
+                <i className="bi bi-plus"></i>
+              </Link>
+            </div>
+
+            <div className="banner-product">
+              <img src={BannerSectionAdmin.src} alt="" />
+              <div className="text-banner">
+                <p>CHÀO MỪNG BẠN ĐẾN VỚI</p>
+                <h1>THE GOLD COFFEE</h1>
+                <p>NĂM 2024</p>
+              </div>
+            </div>
+
+            <div className="order-pending">
+              <div className="title-order-pending">
+                <h1>Danh sách danh mục</h1>
+              </div>
+
+              <div id="list-cate" className="list-order-pending">
+                <div className="title-list-order-pending">
+                  <p>ID</p>
+                  <p>Ảnh danh mục</p>
+                  <p>Tên danh mục</p>
+                  <p>Chức năng</p>
+                </div>
+
+                <div className="main-list">
+                  {cate.map((catea) => (
+                    <div className="main-order-pending">
+                      <Link href="#!">
+                        <p>{catea._id.slice(-4)}</p>
+                      </Link>
+                      <img
+                        src={`${process.env.NEXT_PUBLIC_IMAGE_CATE_URL}${catea.img_cate}`}
+                        alt={catea.name_cate}
+                      />
+                      <p>{catea.name_cate}</p>
+                      <div className="container-func">
+                        <Link href={`/admin/categories/${catea._id}`}>
+                          <button>
+                            <i className="bi bi-gear"></i>
+                          </button>
+                        </Link>
+                        <button onClick={() => deleteCate(catea._id)}>
+                          <i className="bi bi-x-lg"></i>
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default CategoryAdmin;

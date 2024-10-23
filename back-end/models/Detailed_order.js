20d6997a42803ef42d2f553051a4aa74d7585459
minchunp <minhtrung010321@gmail.com>
2024-10-23 09:44:13 +0700

    update

---
+++ back-end/models/Detailed_order.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const detailedOrderSchema = new Schema(
+  {
+    id_detailedOrder: { type: ObjectId }, // khóa chính
+    id_pro: { type: String }, // khóa ngoại tới bảng Product
+    id_order: { type: String }, // khóa ngoại tới bảng Order
+    quantity_detailedOrder: { type: Number }, // số lượng sản phẩm trong chi tiết đơn hàng
+    size_detailedOrder: { type: String }, // kích thước của sản phẩm
+    price_detailedOrder: { type: Number }, // giá của sản phẩm trong chi tiết đơn hàng
+  },
+  { versionKey: false }
+);
+
+module.exports =
+  mongoose.models.DetailedOrder ||
+  mongoose.model("DetailedOrder", detailedOrderSchema);

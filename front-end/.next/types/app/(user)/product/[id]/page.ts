f82544b4981934b5b9c45a5ecaeba1d0e4354a1a
minchunp <minhtrung010321@gmail.com>
2024-10-23 09:07:40 +0700

    up

--- front-end/.next/types/app/(user)/product/[id]/page.ts
+++ front-end/.next/types/app/(user)/product/[id]/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\l\Desktop\TheGoldCoffee\front-end\src\app\(user)\product\[id]\page.tsx
+// File: /Users/macair/Documents/GitHub/TheGoldCoffee/front-end/src/app/(user)/product/[id]/page.tsx
 import * as entry from '../../../../../../src/app/(user)/product/[id]/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
